550fb9008738b9e6804eaf5bc6c1673c61683780
Ayan Mukherjee <ayan.mukherjee22@gmail.com>
2020-09-06 17:34:46 +0530

    Employee Management System project has been created with required dependencies

---
+++ src/test/java/com/socgen/ems/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socgen.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
